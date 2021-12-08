e0cd5302d25841a2569e110d9e967a0aaa306520
Jessica Nelson <yahwehschica@yahoo.com>
2021-12-08 15:27:47 -0500

    commit tree 32c2e489b7cd0310f8cf288846b0842fcd25b84a

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
-}
+  
+    static capitalize(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1)      
+    }
+    static sanitize(string) {
+      return string.replace(/[^A-Za-z0-9']+/g,'',/\s/);
+      }
+    
+    
+    static titleize(string) {
+      const word = string.split(" ")
+      const newWord= word.toUpperCase()
+      for (let i = 0; i < newWord.length; i++) {
+        newWord[i] = newWord[i][0].toUpperCase() + newWord[i].substr(1);
+        return newWord.join(" ");
+    }
+    }
+  }
+  
